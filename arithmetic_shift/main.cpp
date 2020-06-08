bb3f765d91a728668700b82ac0d983c8086710bf
eunyoungroh <roheunyoung@outlook.com>
2020-06-08 15:40:30 +0900

    shift left 추가

--- arithmetic_shift/main.cpp
+++ arithmetic_shift/main.cpp
@@ -1,9 +1,11 @@
 
 #include <stdio.h>
-#define D_WIDTH 8
-#define AMT 2
+#define D_WIDTH 5
+#define AMT 1
+#define RIGHT 100
+#define LEFT 200
  
-int arr[D_WIDTH] = {0, 1, 0, 1, 0, 0, 1, 1}; // 32bit register
+int arr[D_WIDTH] = {1, 0, 1, 1, 1}; // 32bit register
 
 void printArr(){
 	int i;
@@ -13,17 +15,39 @@ void printArr(){
 
 int main(void){
 		
-		int n, i, j, temp;
+		int n, i, j, temp, flag;
 		
 		printf("first: ");
 		printArr();
 		
-		for(j = 0; j < AMT; j++){
-			temp = arr[0];
-			for(i = D_WIDTH - 2; i >= 0; i--){
+		// flag = RIGHT;
+		flag = LEFT;
+		
+		if(flag == RIGHT){
+			printf("shift right\n");
+			// shift right
+			for(j = 0; j < AMT; j++){
+				temp = arr[0];
+				for(i = D_WIDTH - 2; i >= 0; i--)
 					arr[i + 1] = arr[i];
-			}	
-			arr[0] = temp;
+				arr[0] = temp;
+			}		
+		}
+		else if(flag == LEFT) {
+			printf("shift left\n");
+			// shift left
+			for(j = 0; j < AMT; j++){
+				temp = arr[0];
+				for(i = 1; i <= D_WIDTH; i++){
+					if(i == D_WIDTH){
+						arr[D_WIDTH - 1] = 0;
+					}
+					else{
+						arr[i] = arr[i + 1];
+					}
+				}
+				arr[0] = temp;
+			}
 		}
 		
 		printf("result: ");
