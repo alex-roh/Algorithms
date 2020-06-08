1a99a7ce505aa5b08c1c12e539bc541fc302b7c8
eunyoungroh <roheunyoung@outlook.com>
2020-06-08 13:32:07 +0900

    arithmetic-shift

---
+++ arithmetic_shift/main.cpp
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#define D_WIDTH 8
+#define AMT 2
+ 
+int arr[D_WIDTH] = {0, 1, 0, 1, 0, 0, 1, 1}; // 32bit register
+
+void printArr(){
+	int i;
+	for(i = 0; i < D_WIDTH; i++) printf("%d", arr[i]);
+	printf("\n");
+}
+
+int main(void){
+		
+		int n, i, j, temp;
+		
+		printf("초기값: ");
+		printArr();
+		
+		for(j = 0; j < AMT; j++){
+			temp = arr[0];
+			for(i = D_WIDTH - 2; i >= 0; i--){
+					arr[i + 1] = arr[i];
+			}	
+			arr[0] = temp;
+		}
+		
+		printf("결과값: ");
+		printArr();
+ 
+	return 0;
+}
